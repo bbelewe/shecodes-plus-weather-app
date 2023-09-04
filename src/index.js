d6229af9fb52db7989aaf916dc305e44ae001ec6
bbelewe <owenbbaylie@gmail.com>
2023-09-03 21:13:28 -0400

    Finalized Weather Application (see descripiton)
    Got rid of current weather location feature, updated forecast formatting, updated the way the time is displayed, added information at the bottom, and added link to date and time button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const apiKey = "aca4dd3643b89e94dbd3cac6cf6f2638";
+const apiKey = "d67f139b6859d42444fb44355b25ce37";
 const baseUrl = "https://api.openweathermap.org/data/2.5/weather";
 let farenheitTemperature = null;
 
@@ -7,7 +7,7 @@ let now = new Date();
 let buttonSelector = document.querySelector("#date-and-time");
 let date = now.getDate();
 let hours = now.getHours();
-let minutes = now.getMinutes();
+let minutes = now.getMinutes().toString().padStart(2, "0");
 let year = now.getFullYear();
 let days = ["Sun.", "Mon.", "Tues.", "Wed.", "Thurs.", "Fri.", "Sat."];
 let day = days[now.getDay()];
@@ -103,7 +103,7 @@ function fetchWeather(cityName) {
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sunn", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
   return days[day];
 }
 
@@ -111,15 +111,18 @@ function displayForecast(response) {
   let forecast = response.data.daily;
   let forecastElement = document.querySelector("#weather-forecast");
 
+  let currentDay = new Date().getDay();
+
   let forecastHTML = `<div class="row">`;
   forecast.forEach(function (forecastDay, index) {
-    if (index < 7) {
-      forecastHTML += `<div class="col-1">
+    let forecastDayOfWeek = new Date(forecastDay.dt * 1000).getDay();
+    if (index > 0 && forecastDayOfWeek !== currentDay) {
+      forecastHTML += `<div class="col-2">
         <div class="weather-forecast">
           <div class="days alignment">${formatDay(forecastDay.dt)}</div>
           <img src="http://openweathermap.org/img/wn/${
             forecastDay.weather[0].icon
-          }.png"/>
+          }@2x.png" class="forecast-icons" width="75"/>
           <div class="temperatures ocean alignment">
             <span class="tempMax">${Math.round(forecastDay.temp.max)}°</span> /
             <span class="tempMIN">${Math.round(forecastDay.temp.min)}°</span>
@@ -131,52 +134,7 @@ function displayForecast(response) {
 
   forecastHTML += `</div>`;
   forecastElement.innerHTML = forecastHTML;
-
-  function fetchCurrentLocationWeather() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const latitude = position.coords.latitude;
-          const longitude = position.coords.longitude;
-          fetchWeatherByCoords(latitude, longitude);
-        },
-        (error) => {
-          console.error("Error getting current location:", error);
-          alert("An error occurred while getting your current location.");
-        }
-      );
-    } else {
-      console.log("Geolocation is not available in this browser.");
-    }
-  }
-
-  function fetchWeatherByCoords(latitude, longitude) {
-    const url = `${baseUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
-    axios
-      .get(url)
-      .then((response) => {
-        const data = response.data;
-        const temperature = Math.round(data.main.temp);
-        const weatherIcon = data.weather[0].icon;
-        const description = data.weather[0].description;
-        const speed = data.wind.speed;
-
-        mainTemp.innerHTML = `${temperature}°F`;
-        document.querySelector("#city").textContent = "Current Location";
-        document.querySelector(
-          "#current"
-        ).innerHTML = `<img src="https://openweathermap.org/img/wn/${weatherIcon}@4x.png" alt="Weather Icon" class="main-icon">`;
-        document.querySelector(
-          "#descript"
-        ).innerHTML = `Weather Description: ${description}`;
-        document.querySelector(
-          "#speed"
-        ).innerHTML = `Weather Speed: ${speed} Km/H`;
-      })
-
-      .catch((error) => {
-        console.error("Error fetching weather data:", error);
-        alert("An error occurred while fetching weather data.");
-      });
-  }
 }
+window.onload = function () {
+  fetchWeather("New York");
+};
